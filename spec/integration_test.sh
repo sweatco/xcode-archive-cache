9864b81668c3ed27aeda7785c2eb70a85bd6a49b
Ilya Dyakonov <ilushkadyakonov@gmail.com>
2019-05-20 23:34:43 +0300

    test full and partial rebuild with Pods

--- spec/integration_test.sh
+++ spec/integration_test.sh
@@ -1,37 +1,83 @@
 #! /bin/bash
-set -exo
+set -x
+set -o pipefail
 
-check_result() {
+check_for_positive_result() {
   if [ $? != 0 ]; then
-    echo $1 failed
+    echo "\"$1\"" failed
     exit 1
   else
-    echo $1 succeeded
+    echo "\"$1\"" succeeded
+  fi
+}
+
+check_for_negative_result() {
+  if [ $? == 0 ]; then
+    echo "\"$1\"" failed
+    exit 1
+  else
+    echo "\"$1\"" succeeded
   fi
 }
 
 TEST_PROJECT_LOCATION="fixtures/test_project/Test"
 TEST_DESTINATION="platform=iOS Simulator,name=iPhone 7,OS=latest"
 
-perform_test() {
-  cd $TEST_PROJECT_LOCATION
-
-  git clean -xdf -e "build_cache" . && git checkout HEAD -- .
-  check_result "Reset project contents"
-
-  pod install
-  check_result "Install pods"
-
-  xcode-archive-cache inject --destination="$TEST_DESTINATION" --action=build --log-level=verbose
-  check_result "Build and cache dependencies"
-
-  xcodebuild -workspace Test.xcworkspace -scheme Test -destination "$TEST_DESTINATION" -derivedDataPath build test | xcpretty
-  check_result "Test app"
-
-  # check cached dependencies
-  # ...
-
-  cd - > /dev/null
+perform_full_clean() {
+  git clean -xdf . && git checkout HEAD -- .
+  check_for_positive_result "Full clean"
 }
 
-perform_test
+clean_but_leave_build_cache() {
+  git clean -xdf -e "build_cache" . && git checkout HEAD -- .
+  check_for_positive_result "Clean leaving build cache"
+}
+
+make_filename_list_enumerable() {
+  echo "$1" | tr "|" " "
+}
+
+perform_test() {
+  pod install
+  check_for_positive_result "Install pods"
+
+  xcode-archive-cache inject --destination="$TEST_DESTINATION" --action=build --log-level=verbose | tee cache.log
+  check_for_positive_result "Build and cache dependencies"
+
+  # check what was rebuilt
+  #
+  FRAMEWORKS=$(make_filename_list_enumerable "$1")
+  for FRAMEWORK_NAME in $FRAMEWORKS; do
+    grep -q "Touching $FRAMEWORK_NAME" cache.log
+    check_for_positive_result "Rebuild check for $FRAMEWORK_NAME"
+  done
+
+  xcodebuild -workspace Test.xcworkspace -scheme Test -destination "$TEST_DESTINATION" -derivedDataPath build test | xcpretty | tee xcodebuild.log
+  check_for_positive_result "Test app"
+
+  # check that none of cached dependencies
+  # were rebuilt during app build
+  #
+  FRAMEWORKS=$(make_filename_list_enumerable "$2")
+  for FRAMEWORK_NAME in $FRAMEWORKS; do
+    grep -q "Touching $FRAMEWORK_NAME" xcodebuild.log
+    check_for_negative_result "No-extra-rebuild check for $FRAMEWORK_NAME"
+  done
+}
+
+update_single_pod() {
+  sed -i.bak "s+2.5.3+2.5.4+g" Podfile
+  check_for_positive_result "Update single pod"
+}
+
+cd $TEST_PROJECT_LOCATION
+
+ALL_FRAMEWORKS="SDCAutoLayout.framework|RBBAnimation.framework|MRProgress.framework|SDCAlertView.framework|Pods_Test.framework"
+perform_full_clean && perform_test $ALL_FRAMEWORKS $ALL_FRAMEWORKS
+
+# update single pod, expecting it to be rebuilt
+#
+clean_but_leave_build_cache && update_single_pod
+
+FRAMEWORKS_EXPECTED_TO_BE_REBUILT="SDCAutoLayout.framework|SDCAlertView.framework|Pods_Test.framework"
+perform_test $FRAMEWORKS_EXPECTED_TO_BE_REBUILT $ALL_FRAMEWORKS
