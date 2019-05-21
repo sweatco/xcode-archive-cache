916a7139886a4708fbcb9c53059893ef77c653c5
Ilya Dyakonov <ilushkadyakonov@gmail.com>
2019-05-21 18:38:58 +0300

    add library checks to integration test

--- spec/integration_test.sh
+++ spec/integration_test.sh
@@ -22,6 +22,8 @@ check_for_negative_result() {
 
 TEST_PROJECT_LOCATION="fixtures/test_project/Test"
 TEST_DESTINATION="platform=iOS Simulator,name=iPhone 7,OS=latest"
+CACHE_LOG_FILE="cache.log"
+XCODEBUILD_LOG_FILE="xcodebuild.log"
 
 perform_full_clean() {
   git clean -xdf . && git checkout HEAD -- .
@@ -41,18 +43,38 @@ perform_test() {
   pod install
   check_for_positive_result "Install pods"
 
-  xcode-archive-cache inject --destination="$TEST_DESTINATION" --action=build --log-level=verbose | tee cache.log
+  xcode-archive-cache inject --destination="$TEST_DESTINATION" --action=build --log-level=verbose | tee $CACHE_LOG_FILE
   check_for_positive_result "Build and cache dependencies"
 
   # check what was rebuilt
   #
   FRAMEWORKS=$(make_filename_list_enumerable "$1")
   for FRAMEWORK_NAME in $FRAMEWORKS; do
-    grep -q "Touching $FRAMEWORK_NAME" cache.log
+    grep -q "Touching $FRAMEWORK_NAME" $CACHE_LOG_FILE
     check_for_positive_result "Rebuild check for $FRAMEWORK_NAME"
   done
 
-  xcodebuild -workspace Test.xcworkspace -scheme Test -destination "$TEST_DESTINATION" -derivedDataPath build test | xcpretty | tee xcodebuild.log
+  NUMBER_OF_REBUILT_FRAMEWORKS=$(grep "Touching" $CACHE_LOG_FILE | wc -l | xargs)
+  NUMBER_OF_FRAMEWORKS_EXPECTED_TO_BE_REBUILT=$(echo "$FRAMEWORKS" | wc -w | xargs)
+  if [ $NUMBER_OF_REBUILT_FRAMEWORKS != $NUMBER_OF_FRAMEWORKS_EXPECTED_TO_BE_REBUILT ]; then
+    echo "Number of rebuilt frameworks is wrong"
+    exit 1
+  fi
+
+  LIBS=$(make_filename_list_enumerable "$3")
+  for LIB_NAME in $LIBS; do
+    grep -q "Building library $LIB_NAME" $CACHE_LOG_FILE
+    check_for_positive_result "Rebuild check for $LIB_NAME"
+  done
+
+  NUMBER_OF_REBUILT_LIBS=$(grep "Building\slibrary" $CACHE_LOG_FILE | wc -l | xargs)
+  NUMBER_OF_LIBS_EXPECTED_TO_BE_REBUILT=$(echo "$LIBS" | wc -w | xargs)
+  if [ $NUMBER_OF_REBUILT_LIBS != $NUMBER_OF_LIBS_EXPECTED_TO_BE_REBUILT ]; then
+    echo "Number of rebuilt libs is wrong"
+    exit 1
+  fi
+
+  xcodebuild -workspace Test.xcworkspace -scheme Test -destination "$TEST_DESTINATION" -derivedDataPath build test | xcpretty | tee $XCODEBUILD_LOG_FILE
   check_for_positive_result "Test app"
 
   # check that none of cached dependencies
@@ -60,8 +82,14 @@ perform_test() {
   #
   FRAMEWORKS=$(make_filename_list_enumerable "$2")
   for FRAMEWORK_NAME in $FRAMEWORKS; do
-    grep -q "Touching $FRAMEWORK_NAME" xcodebuild.log
+    grep -q "Touching $FRAMEWORK_NAME" $XCODEBUILD_LOG_FILE
     check_for_negative_result "No-extra-rebuild check for $FRAMEWORK_NAME"
+  done
+
+  LIBS=$(make_filename_list_enumerable "$4")
+  for LIB_NAME in $LIBS; do
+    grep -q "Building library $LIB_NAME" $XCODEBUILD_LOG_FILE
+    check_for_negative_result "No-extra-rebuild check for $LIB_NAME"
   done
 }
 
@@ -72,12 +100,13 @@ update_single_pod() {
 
 cd $TEST_PROJECT_LOCATION
 
-ALL_FRAMEWORKS="SDCAutoLayout.framework|RBBAnimation.framework|MRProgress.framework|SDCAlertView.framework|Pods_Test.framework"
-perform_full_clean && perform_test $ALL_FRAMEWORKS $ALL_FRAMEWORKS
+ALL_FRAMEWORKS="SDCAutoLayout.framework|RBBAnimation.framework|MRProgress.framework|SDCAlertView.framework|Pods_Test.framework|FrameworkDependency.framework"
+ALL_LIBS="libLibraryWithFrameworkDependency.a|libStaticDependency.a"
+perform_full_clean && perform_test $ALL_FRAMEWORKS $ALL_FRAMEWORKS $ALL_LIBS $ALL_LIBS
 
 # update single pod, expecting it to be rebuilt
 #
 clean_but_leave_build_cache && update_single_pod
 
-FRAMEWORKS_EXPECTED_TO_BE_REBUILT="SDCAutoLayout.framework|SDCAlertView.framework|Pods_Test.framework"
-perform_test $FRAMEWORKS_EXPECTED_TO_BE_REBUILT $ALL_FRAMEWORKS
+FRAMEWORKS_EXPECTED_TO_BE_REBUILT="SDCAlertView.framework|Pods_Test.framework"
+perform_test $FRAMEWORKS_EXPECTED_TO_BE_REBUILT $ALL_FRAMEWORKS "" $ALL_LIBS
