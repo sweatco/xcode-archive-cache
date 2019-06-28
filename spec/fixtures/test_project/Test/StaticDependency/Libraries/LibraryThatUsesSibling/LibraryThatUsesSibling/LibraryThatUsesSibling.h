2d9d9f7441449bab4b07e56e11987fa9c156c66a
Ilya Dyakonov <ilushkadyakonov@gmail.com>
2019-06-28 19:37:44 +0300

    add dependency that uses sibling to test project

---
+++ spec/fixtures/test_project/Test/StaticDependency/Libraries/LibraryThatUsesSibling/LibraryThatUsesSibling/LibraryThatUsesSibling.h
@@ -0,0 +1,14 @@
+//
+//  LibraryThatUsesSibling.h
+//  LibraryThatUsesSibling
+//
+//  Created by Ilya Dyakonov on 6/28/19.
+//  Copyright © 2019 xcode-archive-cache. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+// to be removed during tests: #import <LibraryWithFrameworkDependency/LibraryWithFrameworkDependency.h>
+
+@interface LibraryThatUsesSibling : NSObject
+
+@end
---
+++ spec/fixtures/test_project/Test/StaticDependency/Libraries/LibraryThatUsesSibling/LibraryThatUsesSibling/LibraryThatUsesSibling.m
@@ -0,0 +1,13 @@
+//
+//  LibraryThatUsesSibling.m
+//  LibraryThatUsesSibling
+//
+//  Created by Ilya Dyakonov on 6/28/19.
+//  Copyright © 2019 xcode-archive-cache. All rights reserved.
+//
+
+#import "LibraryThatUsesSibling.h"
+
+@implementation LibraryThatUsesSibling
+
+@end
