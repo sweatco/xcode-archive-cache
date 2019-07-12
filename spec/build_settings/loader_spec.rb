02a5428b408c136bec45a4b1bc4694974fdf63e2
Ilya Dyakonov <ilushkadyakonov@gmail.com>
2019-07-12 19:52:58 +0300

    fix build settings loader spec

--- spec/build_settings/loader_spec.rb
+++ spec/build_settings/loader_spec.rb
@@ -7,29 +7,29 @@ RSpec.describe XcodeArchiveCache::BuildSettings::Loader, "#load_build_settings" 
   it "should save settings per project" do
     first_project_settings = "Build settings for action archive target first\n" \
     "  TARGETNAME = first\n" \
-    "  ARCH = armv7\n" \
+    "  ARCHS = armv7\n" \
     "  PATH = some path\n" \
     "Build settings for action archive target second\n" \
     "  TARGETNAME = second\n" \
-    "  ARCH = arm64\n" \
+    "  ARCHS = arm64\n" \
     "  PATH = some path\n"
     allow(@executor).to receive(:load_build_settings).with("first_project_path").and_return(first_project_settings)
     second_project_settings = "Build settings for action archive target first\n" \
     "  TARGETNAME = first\n" \
-    "  ARCH = armv7s\n" \
+    "  ARCHS = armv7s\n" \
     "  PATH = some path for second project\n" \
     "Build settings for action archive target second\n" \
     "  TARGETNAME = second\n" \
-    "  ARCH = arm64\n" \
+    "  ARCHS = arm64\n" \
     "  PATH = some path\n"
     allow(@executor).to receive(:load_build_settings).with("second_project_path").and_return(second_project_settings)
 
     @loader.load_settings("first_project_path")
     @loader.load_settings("second_project_path")
 
-    settings_container = XcodeArchiveCache::BuildSettings::Container.new({"TARGETNAME" => "first", "ARCH" => "armv7", "PATH" => "some path"}, {"TARGETNAME" => "first", "ARCH" => "armv7"})
+    settings_container = XcodeArchiveCache::BuildSettings::Container.new({"TARGETNAME" => "first", "ARCHS" => "armv7", "PATH" => "some path"}, {"TARGETNAME" => "first", "ARCHS" => "armv7"})
     expect(@loader.get_settings("first_project_path", "first")).to eq(settings_container)
-    settings_container = XcodeArchiveCache::BuildSettings::Container.new({"TARGETNAME" => "first", "ARCH" => "armv7s", "PATH" => "some path for second project"}, {"TARGETNAME" => "first", "ARCH" => "armv7s"})
+    settings_container = XcodeArchiveCache::BuildSettings::Container.new({"TARGETNAME" => "first", "ARCHS" => "armv7s", "PATH" => "some path for second project"}, {"TARGETNAME" => "first", "ARCHS" => "armv7s"})
     expect(@loader.get_settings("second_project_path", "first")).to eq(settings_container)
   end
 end
