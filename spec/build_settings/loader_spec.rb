3a1a262c9af07f74983f0f518fd48be079425277
Ilya Dyakonov <ilushkadyakonov@gmail.com>
2020-04-13 15:38:21 +0300

    fix unit tests

--- spec/build_settings/loader_spec.rb
+++ spec/build_settings/loader_spec.rb
@@ -24,6 +24,7 @@ RSpec.describe XcodeArchiveCache::BuildSettings::Loader, "#load_build_settings" 
     "  PATH = some path\n"
     allow(@executor).to receive(:load_build_settings).with("second_project_path").and_return(second_project_settings)
     allow(@executor).to receive_messages(:arguments_state => "arguments_state")
+    allow(@executor).to receive_messages(:set_up_for_simulator? => false)
 
     @loader.load_settings(%w(first_project_path second_project_path))
 
