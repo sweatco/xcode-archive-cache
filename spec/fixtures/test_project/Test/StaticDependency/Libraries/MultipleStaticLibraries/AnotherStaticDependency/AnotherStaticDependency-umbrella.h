1023bd4b74d990494b2b46ea2792be83a9b1fd2e
Ilya Dyakonov <ilushkadyakonov@gmail.com>
2020-04-13 14:04:13 +0300

    turn static dependency of test project into Clang module

---
+++ spec/fixtures/test_project/Test/StaticDependency/Libraries/MultipleStaticLibraries/AnotherStaticDependency/AnotherStaticDependency-umbrella.h
@@ -0,0 +1,14 @@
+//
+//  AnotherStaticDependency-umbrella.h
+//  MultipleStaticLibraries
+//
+//  Created by Ilya Dyakonov on 4/2/20.
+//  Copyright © 2020 xcode-archive-cache. All rights reserved.
+//
+
+#ifndef AnotherStaticDependency_umbrella_h
+#define AnotherStaticDependency_umbrella_h
+
+#import "AnotherStaticDependency.h"
+
+#endif /* AnotherStaticDependency_umbrella_h */
