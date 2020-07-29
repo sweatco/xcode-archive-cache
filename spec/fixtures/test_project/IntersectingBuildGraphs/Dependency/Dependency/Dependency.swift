c1bfa37461e557351df6613793b4e6fa436a5500
Ilya Dyakonov <ilushkadyakonov@gmail.com>
2020-07-29 18:20:41 +0300

    add test project with intersecting build graphs

---
+++ spec/fixtures/test_project/IntersectingBuildGraphs/Dependency/Dependency/Dependency.swift
@@ -0,0 +1,11 @@
+//
+//  Dependency.swift
+//  Dependency
+//
+//  Created by Ilya Dyakonov on 7/29/20.
+//  Copyright © 2020 xcode-archive-test. All rights reserved.
+//
+
+class Dependency {
+
+}
---
+++ spec/fixtures/test_project/IntersectingBuildGraphs/IntersectingBuildGraphs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IntersectingBuildGraphs
+//
+//  Created by Ilya Dyakonov on 7/29/20.
+//  Copyright © 2020 xcode-archive-test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
