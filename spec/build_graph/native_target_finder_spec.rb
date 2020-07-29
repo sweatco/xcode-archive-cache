5f2797b42a3c56e1e3c217c1c5b0f6eba10a2fd9
Ilya Dyakonov <ilushkadyakonov@gmail.com>
2020-07-29 11:36:32 +0300

    add project doubling to target finder test cases

--- spec/build_graph/native_target_finder_spec.rb
+++ spec/build_graph/native_target_finder_spec.rb
@@ -2,6 +2,7 @@ RSpec.describe XcodeArchiveCache::BuildGraph::NativeTargetFinder, "#find" do
   describe "valid searches" do
     before :each do
       @project = double("project")
+      allow(@project).to receive(:path).and_return("/project")
 
       @first_native_target = double("first_native_target")
       @first_product_ref = double("first_product_ref")
@@ -16,6 +17,58 @@ RSpec.describe XcodeArchiveCache::BuildGraph::NativeTargetFinder, "#find" do
       allow(@second_native_target).to receive(:product_reference).and_return(@second_product_ref)
       allow(@second_native_target).to receive(:platform_name).and_return("platform")
       allow(@second_native_target).to receive(:test_target_type?).and_return(false)
+    end
+
+    describe "with doubled project" do
+      before :each do
+        @sibling_project = double("sibling_project")
+        allow(@sibling_project).to receive(:path).and_return("/project/sibling")
+
+        sibling_project_ref = double("sibling_project_ref")
+        allow(sibling_project_ref).to receive(:path).and_return("sibling.xcodeproj")
+        allow(sibling_project_ref).to receive(:real_path).and_return("sibling.xcodeproj")
+
+        @nested_project = double("nested_project")
+        allow(@nested_project).to receive(:path).and_return("/project/nested")
+
+        nested_project_ref = double("nested_project_ref")
+        allow(nested_project_ref).to receive(:path).and_return("nested.xcodeproj")
+        allow(nested_project_ref).to receive(:real_path).and_return("nested.xcodeproj")
+        allow(@project).to receive(:files).and_return([nested_project_ref])
+
+        allow(File).to receive(:exist?) do |file_path| 
+          file_path == "nested.xcodeproj" || file_path == "sibling.xcodeproj"
+        end
+
+        allow(Xcodeproj::Project).to receive(:open) do |file_path|
+          if file_path == "nested.xcodeproj"
+            @nested_project
+          elsif file_path == "sibling.xcodeproj"
+            @sibling_project
+          end
+        end
+
+        @file_ref = double("file_ref")
+        allow(@file_ref).to(receive(:is_a?)) {|klass| klass == Xcodeproj::Project::Object::PBXFileReference}
+        allow(@file_ref).to receive(:path).and_return("/some/path")
+
+        @file = double("file")
+        allow(@file).to receive(:file_ref).and_return(@file_ref)
+
+        allow(@sibling_project).to receive(:native_targets).and_return([@first_native_target])
+        allow(@sibling_project).to receive(:files).and_return([])
+        allow(@nested_project).to receive(:files).and_return([sibling_project_ref])
+        allow(@project).to receive(:files).and_return([nested_project_ref])
+        allow(@project).to receive(:native_targets).and_return([])
+        allow(@nested_project).to receive(:native_targets).and_return([])
+      end
+
+      it "should not discover a target from doubled project multiple times" do
+        finder = XcodeArchiveCache::BuildGraph::NativeTargetFinder.new([@project])
+        finder.set_platform_name_filter("platform")
+
+        expect(finder.find_for_file(@file)).to eq(@first_native_target)
+      end
     end
 
     describe "on file ref" do
@@ -45,6 +98,7 @@ RSpec.describe XcodeArchiveCache::BuildGraph::NativeTargetFinder, "#find" do
         allow(@project).to receive(:files).and_return([nested_project_ref])
 
         nested_project = double("nested_project")
+        allow(nested_project).to receive(:path).and_return("/project/nested")
         allow(nested_project).to receive(:native_targets).and_return([@second_native_target])
         allow(nested_project).to receive(:files).and_return([])
 
@@ -82,6 +136,8 @@ RSpec.describe XcodeArchiveCache::BuildGraph::NativeTargetFinder, "#find" do
         allow(@nested_project_ref).to receive(:real_path).and_return("nested.xcodeproj")
 
         @nested_project = double("nested_project")
+        allow(@nested_project).to receive(:path).and_return("/project/nested")
+
         product_reference = double("product_reference")
         allow(product_reference).to receive(:uuid).and_return("product_uuid")
         allow(@second_native_target).to receive(:product_reference).and_return(product_reference)
