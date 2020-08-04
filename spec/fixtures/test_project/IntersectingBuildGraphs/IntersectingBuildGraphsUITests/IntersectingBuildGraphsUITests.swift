17c10ae7598e56dd5433a9d015b528e17b0f97ba
Ilya Dyakonov <ilushkadyakonov@gmail.com>
2020-08-04 11:15:40 +0300

    clean up static lib modules ui tests

--- spec/fixtures/test_project/IntersectingBuildGraphs/IntersectingBuildGraphsUITests/IntersectingBuildGraphsUITests.swift
+++ spec/fixtures/test_project/IntersectingBuildGraphs/IntersectingBuildGraphsUITests/IntersectingBuildGraphsUITests.swift
@@ -9,21 +9,12 @@
 import XCTest
 
 class IntersectingBuildGraphsUITests: XCTestCase {
-    override func setUpWithError() throws {
+    override func setUp() {
         continueAfterFailure = false
     }
 
-    func testAppLaucnch() throws {
+    func testAppLaunch() {
         let app = XCUIApplication()
         app.launch()
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
-        }
     }
 }
