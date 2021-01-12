78f5c3935a02906a6568648f1941da0bcaa1bfac
Ilya Dyakonov <ilushkadyakonov@gmail.com>
2021-01-12 15:44:06 +0300

    add test target to introduce modulemap path clash

---
+++ spec/fixtures/test_project/StaticLibModules/SomethingWithLottie/SomethingWithLottie.m
@@ -0,0 +1,19 @@
+//
+//  SomethingWithLottie.m
+//  SomethingWithLottie
+//
+//  Created by Ilya Dyakonov on 1/12/21.
+//  Copyright © 2021 xcode-archive-cache. All rights reserved.
+//
+
+#import "SomethingWithLottie.h"
+
+@import Lottie;
+
+@implementation SomethingWithLottie
+
++(void) doSomethingWithLottie {
+    NSLog(@"%@", LottieView.description);
+}
+
+@end
