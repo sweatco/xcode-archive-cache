a74b7b85332ce9b7074486c42958e3571e9c5aab
Ilya Dyakonov <ilushkadyakonov@gmail.com>
2022-09-14 21:15:36 +0100

    add fixtures for sibling projects case

---
+++ spec/fixtures/test_project/SiblingProjects/SiblingProjects/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  SiblingProjects
+//
+//  Created by Ilya Dyakonov on 14.09.2022.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ spec/fixtures/test_project/SiblingProjects/SiblingProjects/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  SiblingProjects
+//
+//  Created by Ilya Dyakonov on 14.09.2022.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
